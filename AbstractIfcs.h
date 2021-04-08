cfd59b47d9b6fa788d6a6f9d399d91f980c1dba1
e-ing <dmitriy.i-sys@yandex.ru>
2021-04-08 18:22:08 +0700

    AbstractIfcs v1

--- AbstractIfcs.h
+++ AbstractIfcs.h
@@ -1,5 +1,7 @@
-#pragma once
-
+#ifndef ABSTRACT_IFCS_H
+#define ABSTRACT_IFCS_H
+//v1-08.04.21
+//input
 class Abstract_iPort
 {
 public:
@@ -23,7 +25,7 @@ public:
 	virtual void PullH() = 0;
 	virtual void PullD() = 0;
 	virtual void SetSpeed(unsigned int speed) = 0;
-	~Abstract_iBit() {}
+	virtual ~Abstract_iBit() {}
 };
 
 //ADCs  and etc
@@ -38,7 +40,7 @@ public:
 		return *this;
 	}
 	virtual operator int() { return GetValue(); }
-	~Abstract_iInt() {}
+	virtual ~Abstract_iInt() {}
 };
 
 //serial ports in and etc
@@ -49,39 +51,91 @@ protected:
 public:
 	virtual Abstract_iPort& operator >> (void* any)
 	{
-		char* any = GetStr();
+		any = GetStr();
 		return *this;
 	}
 	virtual operator char* () { return GetStr(); }
 	virtual int GetDataLen() = 0;
-	~Abstract_iString() {}
+	virtual ~Abstract_iString() {}
 };
 
-//Memorys in  and etc
+////Memorys in  and etc
 
 class Abstract_iArray : public  Abstract_iPort
 {
 	unsigned int index;
 	unsigned int rdSz;
-	virtual unsigned int GetData(unsigned int idx, int* data, int sz) = 0;
+	virtual unsigned int GetData(unsigned int idx, void* data, unsigned int sz) = 0;
 	virtual unsigned int GetData(unsigned int idx) = 0;
 public:
 	virtual void SetIdx(unsigned int idx) { index = idx; }
 	virtual unsigned int operator[] (unsigned int idx)
 	{
-		return GetData(index++);
+		return GetData(index);
 	}
 	virtual unsigned int GetIdx() { return index; }
 	void SetSz(unsigned int sz) { rdSz = sz; }
 	virtual Abstract_iPort& operator >> (void* any) 
 	{
-		for (int i = rdSz; i > 0; --i)
-			(int*)any = GetData(index++);
+			GetData(index, any, rdSz);
+		return *this;
 	}
 
 	virtual operator int()
 	{
-		return GetData(index++);
+		return GetData(index);
 	}
-	~Abstract_iArray() {}
+	virtual ~Abstract_iArray() {}
 };
+//out
+class Abstract_oPort
+{
+public:
+	virtual Abstract_oPort& operator << (void* any) = 0;
+	virtual ~Abstract_oPort() {}
+};
+
+//GPout and etc
+class Abstract_oBit : public  Abstract_oPort
+{
+protected:
+	virtual bool GetState() = 0;
+	virtual void SetValue( bool x) =0;
+	virtual void Toggle () = 0;
+	virtual void SetState(void* state)
+	{
+		SetValue(*((bool*) state));
+	}
+public:
+	virtual void Set() = 0;
+	virtual void Clear() = 0;
+	virtual void SetSpeed(unsigned int speed) = 0;
+	
+	virtual Abstract_oPort& operator << (void* any)
+	{
+		SetState(any);
+		return *this;
+	}
+	virtual operator bool() { return GetState(); }
+	virtual operator int() { return (GetState() == false) ? 0 : 1; };
+	virtual Abstract_oBit&  operator = (bool val) 
+	{
+		SetValue(val);
+		return *this;
+	} 
+	virtual Abstract_oBit& operator = (int val) 
+	{
+		SetValue((val==0)? false : true);
+		return *this;
+	}
+	virtual Abstract_oBit& operator !()
+	{
+		Toggle();
+		return *this;
+	}
+	virtual ~Abstract_oBit() {}
+};
+
+
+#endif
+
